a9042910c997c75af6e77043d92148af49f372d3
PavKolacek <pavekolacek@gmail.com>
2025-11-18 08:30:00 +0000

    prubezne fixy

--- fungovani_automatu/backend_automatu.cpp
+++ fungovani_automatu/backend_automatu.cpp
@@ -2,8 +2,7 @@
 #include "../jednotlive_hry/magic8.h"
 #include "../jednotlive_hry/snake.h"
 #include "../jednotlive_hry/sortinghat.h"
-#include "../jednotlive_hry/tic_tac_toe.cpp"
-#include "animace.cpp"
+//#include "tic-tac-toe.cpp"
 
 int vyber_hry1(){
     int vyberHry;
@@ -12,28 +11,18 @@ int vyber_hry1(){
     switch (vyberHry)
     {
     case 1:
-         hadej_cislo();
+        hadej_cislo();
         break;
     case 2:
-         magic8();
+        magic8();
         break;
     case 3:
-         sorting_hat();
+        sorting_hat();
         break;
     case 4:
         snake();
-        break;
-    case 5:
-         tic_tac_toe();
     default:
         break;
     }
-    return 0;
-};
-
-
-
-int backend_automatu(){
-    vyber_hry1();
     return 0;
 }
